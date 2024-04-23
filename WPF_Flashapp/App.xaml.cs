aff1c23c6d705cdf2e009f62ed3847307ca9f5fc
Vu Nam <vnam0320@gmail.com>
2024-04-23 23:31:33 +0300

    Add project files.

---
+++ WPF_Flashapp/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using WPF_Flashapp.Views.Windows;
+
+namespace WPF_Flashapp
+{
+    public partial class App : Application
+    {
+        private ServiceCollection _service = new ServiceCollection();
+        private ServiceProvider _provider;
+        public App()
+        {
+            _service.AddSingleton<MainWindow>();
+            _provider = _service.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var main = _provider.GetRequiredService<MainWindow>();
+            main.Show();
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ WPF_Flashapp/Views/UserControls/WindowTitle.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WPF_Flashapp.MVVM;
+
+namespace WPF_Flashapp.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for WindowTitle.xaml
+    /// </summary>
+    public partial class WindowTitle : UserControl
+    {
+        public RelayCommand CloseWindow => new RelayCommand(execute => Application.Current.Shutdown());
+        public RelayCommand MinimizeWindow => new RelayCommand(execute => Application.Current.MainWindow.WindowState = WindowState.Minimized);
+        public RelayCommand MaximizeWindow => new RelayCommand(execute => maximizeWindow());
+        public WindowTitle()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+        private void maximizeWindow()
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+    }
+}
